36d6bbff550c27ac04f21f62731ee1fbc0d14470
Kenza Kharraz <Kenza.kharraz5@gmail.com>
2025-11-30 21:41:56 +0100

    Control Commit

--- billing-service/src/main/java/com/kharraz/billingservice/repository/BillRepository.java
+++ billing-service/src/main/java/com/kharraz/billingservice/repository/BillRepository.java
@@ -2,6 +2,8 @@ package com.kharraz.billingservice.repository;
 
 import com.kharraz.billingservice.entities.Bill;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
-public interface BillRepository  extends JpaRepository<Bill, Long> {
+@RepositoryRestResource
+public interface BillRepository extends JpaRepository<Bill, Long> {
 }
